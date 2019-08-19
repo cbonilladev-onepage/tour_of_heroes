db4bba368b274767776ecb5fdd673ec8b913b50a
cbonilla <christian.bonilla01@gmail.com>
2019-08-19 15:33:52 -0700

    Angular Hero list display and onClick action.

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Cyclops', description: "The reluctant leader of the X-Men who shoots concussive blasts from his eyes, Scott Summers has been fighting for the peaceful coexistence of mutants and humans most of his life."},
+    { id: 12, name: 'Wolverine', description: "With a past shrouded in mystery, Wolverine's memories are full of government secrets, traumatic events, and death."},
+    { id: 13, name: 'Magneto', description: "Max Eisenhardt aka 'Magneto' the 'Master of Magnetism' is a powerful Mutant who has the ability to generate and control magnetic fields."},
+    { id: 14, name: 'Spiderman', description: "As a result of a radioactive spider bite, high schooler Peter Parker developed powers and abilities similar to that of a spider."},
+    { id: 15, name: 'Thanos', description: "Thanos was one of the last sons of A'Lars, progenitor of the second colony of Eternals on Titan, and Sui-San, the last survivor of the original settlement of Eternals in this moon."},
+    { id: 16, name: 'Iron Man', description: "Genius billionaire inventor, industrialist and CEO of Stark Industries Tony Stark was fatally wounded in a war zone in Afghanistan shortly before being kidnapped by terrorists."},
+    { id: 17, name: 'Deadpool', description: "Wade Wilson was an international assassin who had worked for various governments when he developed an aggressive cancer."},
+    { id: 18, name: 'Gambit', description: "Abandoned at birth due to his burning red eyes, the mutant child who would one day become Remy LeBeau was kidnapped from his hospital ward by members of the New Orleans Thieves Guild who referred to the child as 'le diable blanc' - the white devil."},
+    { id: 19, name: 'Human Torch', description: "Two men use the alias of The Human Torch in the mainstream continuity: Jonathan Storm, the youngest member of the Fantastic Four, who gained super powers from cosmic radiation as well as the other three members during an unauthorized space flight, doctor Reed Richards, his sister Sue Storm and pilot Ben Grimm."},
+    { id: 20, name: 'Professor X', description: "Charles Francis Xavier had a dream, where mutants and humans lived together in perfect harmony and peace, as equals and used his family's fortune to found Xavier's School for Gifted Youngsters, the X-Men, New Mutants, and even Generation X, to see this dream through."},
+]
